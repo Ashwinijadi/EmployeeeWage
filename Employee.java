535a9bf317ebe1704cfdd30864291719dd266c14
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-24 19:05:37 +0530

    Added: Daily Employee Wage is calculated

--- Employee.java
+++ Employee.java
@@ -1,23 +1,30 @@
 package Employee;
 
 public class Employee {
-	 public static void main(String[] args) {
-    	 //TODO Auto-generated method stub
-    	 
-    	 System.out.println("Welcome to Employee Wage Computation Program");
-    	 
-    	 //Constants
-    	 int IS_FULL_TIME=1;
-    	 
-    	 //Computation
-    	 double empCheck=Math.floor(Math.random() * 10) % 2;
-    	 if(empCheck == IS_FULL_TIME) {
- 			System.out.print("Employee present");
-    	 }
-    	 else {
- 			System.out.print("Employee absent");
- 		}   	 
-    	 
-     }
+	public static void main(String[] args) { 	
+	      //TODO Auto-generated method stub
+			
+			System.out.println("Welcome to Employee Wage Computation Program");	
+		   	 //Constants
+		   	 int IS_FULL_TIME=1;
+		   	int EMP_RATE_PER_HOUR=20;	   	 
+		      //Variable
+		  		int empWage;
+		  		int empHours=0;
+		      //Computation
+		   	 double empCheck=Math.floor(Math.random() * 10) % 2;
+		   	 if(empCheck == IS_FULL_TIME) {
+					System.out.println("Employee present");
+					 empHours=8;
+		   	 }
+		   	 else {
+		   		 empHours=0;
+					System.out.println("Employee absent");
+				}  
+		   	 //Wage Calculation
+		   	empWage =empHours * EMP_RATE_PER_HOUR;
+			System.out.println("Employee Daily Wage: "  +empWage);
+		   	 
+		    }
 }
 
