0dc8137c27c104c9e39f93be078258d275e47fad
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-24 19:19:39 +0530

    Added: Parttime Employees Added

--- Employee.java
+++ Employee.java
@@ -1,30 +1,32 @@
 package Employee;
 
 public class Employee {
-	public static void main(String[] args) { 	
-	      //TODO Auto-generated method stub
-			
-			System.out.println("Welcome to Employee Wage Computation Program");	
-		   	 //Constants
-		   	 int IS_FULL_TIME=1;
-		   	int EMP_RATE_PER_HOUR=20;	   	 
-		      //Variable
-		  		int empWage;
-		  		int empHours=0;
-		      //Computation
-		   	 double empCheck=Math.floor(Math.random() * 10) % 2;
-		   	 if(empCheck == IS_FULL_TIME) {
-					System.out.println("Employee present");
-					 empHours=8;
-		   	 }
-		   	 else {
-		   		 empHours=0;
-					System.out.println("Employee absent");
-				}  
-		   	 //Wage Calculation
-		   	empWage =empHours * EMP_RATE_PER_HOUR;
-			System.out.println("Employee Daily Wage: "  +empWage);
-		   	 
-		    }
+    //Constants
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=100;
+	public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation Program");
+	//Variable
+	int empWage=0;
+	int empHours=0;
+	int empCheck=(int)Math.floor(Math.random() * 10) %3;
+	System.out.println("employeeCheck : "+empCheck);
+	if(empCheck == IS_PART_TIME) {
+	   System.out.println("Parttime Employee is present");
+	   empHours =4;
+	}
+	else if(empCheck == IS_FULL_TIME) {
+		System.out.println("Fulltime Employee is present");
+	    empHours=8;
+	}
+	else {
+		empHours=0;
+		System.out.println("Employee absent");
+	}
+	//computation
+	empWage =empHours * EMP_RATE_PER_HOUR;
+	System.out.println("Employee Daily Wage: "  +empWage);
+}
 }
 
