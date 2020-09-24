b25ba62058389370d1a69d27d2efea45ba3735ff
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-09-24 19:32:44 +0530

    Modified: Switchcase is used

--- Employee.java
+++ Employee.java
@@ -1,32 +1,44 @@
 package Employee;
 
 public class Employee {
-    //Constants
+	//Constants
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=100;
 	public static void main(String[] args) {
+		
 	System.out.println("Welcome to Employee Wage Computation Program");
+
 	//Variable
 	int empWage=0;
 	int empHours=0;
-	int empCheck=(int)Math.floor(Math.random() * 10) %3;
-	System.out.println("employeeCheck : "+empCheck);
-	if(empCheck == IS_PART_TIME) {
-	   System.out.println("Parttime Employee is present");
+	
+	int employeeCheck=(int)Math.floor(Math.random() * 10) %3;
+	System.out.println("employeeCheck : "+employeeCheck);		
+	switch (employeeCheck) {
+	case IS_PART_TIME:
+		empHours=4;
+		break;
+	case IS_FULL_TIME:
+		empHours=8;
+		break;
+	default:
+		empHours=0;
+	}
+	if(employeeCheck == IS_PART_TIME) {
+	   System.out.println("Parttime Employee present");
 	   empHours =4;
 	}
-	else if(empCheck == IS_FULL_TIME) {
-		System.out.println("Fulltime Employee is present");
+	else if(employeeCheck == IS_FULL_TIME) {
+		System.out.println("Fulltime Employee present");
 	    empHours=8;
 	}
 	else {
 		empHours=0;
 		System.out.println("Employee absent");
 	}
-	//computation
 	empWage =empHours * EMP_RATE_PER_HOUR;
-	System.out.println("Employee Daily Wage: "  +empWage);
+	System.out.println("Daily Employee Wage: "  +empWage);
 }
 }
 
